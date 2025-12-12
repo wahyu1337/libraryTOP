39c2122d977340f1a5482e9f1015a46429d817e4
wahyu1337 <wahyuisnaeni2002@gmail.com>
2025-12-13 07:19:58 +0800

    ADD constructor & Info prototype

--- script.js
+++ script.js
@@ -1,12 +1,24 @@
-// script.js
-document.addEventListener('DOMContentLoaded', function() {
-    // Get all read status elements
-    const readStatusElements = document.querySelectorAll('.mainContentReadStatus p');
-    
-    readStatusElements.forEach(function(element) {
-        element.addEventListener('click', function() {
-            // Toggle the 'read-toggle' class
-            this.classList.toggle('read-toggle');
-        });
-    });
-});
+const myLibrary = [];
+
+// Constructor
+function Book(title, author, pages, status, cover){
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.status = status;
+    this.cover = cover;
+}
+
+// get book info (with prototype)
+Book.prototype.getInfo = function(){
+    console.log(`
+        Title: ${this.title}
+        Author: ${this.author}
+        Pages: ${this.pages}
+        Status ${this.status}`)
+};
+
+const book1 = new Book("Harry Potters", "J.K Rowwling", "287", "READ");
+const book2 = new Book("The Witcher 3", "Andrzej Sapkowski", "230", "UnREAD");
+book1.getInfo();
+book2.getInfo();
